4fb43a8e884341d11e777026fa12c34b07926bc3
Chanwut Kittivorawong <chanwutk@cs.washington.edu>
2020-04-11 16:27:42 -0700

    Co-authored-by: podshara <podsharafriend@hotmail.com>

--- labs/lab2-primarybackup/src/dslabs/primarybackup/ViewServer.java
+++ labs/lab2-primarybackup/src/dslabs/primarybackup/ViewServer.java
@@ -4,6 +4,7 @@ import dslabs.framework.Address;
 import dslabs.framework.Node;
 import dslabs.primarybackup.View;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -17,9 +18,10 @@ class ViewServer extends Node {
     private static final int INITIAL_VIEWNUM = 1;
 
     // Your code here...
-    private View currentView = new View(STARTUP_VIEWNUM, null, null);
-    private Set<Address> aliveServers = new HashSet<Address>();
-    private Set<Address> prevAliveServers = new HashSet<Address>();
+    private View currentView;
+    private Set<Address> aliveServers;
+    private Set<Address> prevAliveServers;
+    private boolean ack;
 
     /* -------------------------------------------------------------------------
         Construction and Initialization
@@ -32,21 +34,10 @@ class ViewServer extends Node {
     public void init() {
         set(new PingCheckTimer(), PING_CHECK_MILLIS);
         // Your code here...
-        // currentView = new View(STARTUP_VIEWNUM, null, null);
-        // aliveServers = new HashSet<Address>();
-        // prevAliveServers = new HashSet<Address>();
-        // Address primary = currentView.primary();
-        // Address backup = currentView.backup();
-        // for (Address server : aliveServers) {
-        //     if (primary == null) {
-        //         primary = server;
-        //     } else if (backup == null) {
-        //         backup = server;
-        //     } else {
-        //         break;
-        //     }
-        // }
-        // currentView = new View(INITIAL_VIEWNUM, primary, backup);
+        currentView = new View(STARTUP_VIEWNUM, null, null);
+        aliveServers = new HashSet<Address>();
+        prevAliveServers = new HashSet<Address>();
+        ack = false;
     }
 
     /* -------------------------------------------------------------------------
@@ -56,17 +47,22 @@ class ViewServer extends Node {
         // Your code here...
         System.out.println(sender);
         aliveServers.add(sender);
-        if (currentView.viewNum() == STARTUP_VIEWNUM) {
+
+        Address primary = currentView.primary();
+        int currentViewNum = currentView.viewNum();
+
+        ack = Objects.equals(primary, sender) && m.viewNum() == currentViewNum;
+
+        if (currentViewNum == STARTUP_VIEWNUM) {
             currentView = new View(INITIAL_VIEWNUM, sender, null);
-        } else if (currentView.backup() == null && sender.compareTo(currentView.primary()) != 0) {
-            currentView = new View(currentView.viewNum() + 1, currentView.primary(), sender);
+        } else if (currentView.backup() == null && !Objects.equals(sender, primary) && ack) {
+            currentView = new View(currentViewNum + 1, primary, sender);
         }
         send(new ViewReply(currentView), sender);
     }
 
     private void handleGetView(GetView m, Address sender) {
         // Your code here...
-        System.out.println(currentView);
         send(new ViewReply(currentView), sender);
     }
 
@@ -77,18 +73,23 @@ class ViewServer extends Node {
         // Your code here...
         Address primary = currentView.primary();
         Address backup = currentView.backup();
-        if (!serverAlive(primary)) {
-            if (!serverAlive(backup)) {
-                // TODO: dies?
-                throw new IllegalArgumentException();
-            } else {
-                replaceBackup(backup);
+        int viewNum = currentView.viewNum();
+        System.out.println(currentView);
+        if (viewNum != STARTUP_VIEWNUM && ack) {
+            System.out.println(serverAlive(primary) + " " + serverAlive(backup));
+            if (!serverAlive(primary)) {
+                if (!serverAlive(backup)) {
+                    // TODO: dies?
+                    throw new IllegalArgumentException();
+                } else {
+                    replaceBackup(backup);
+                }
+            } else if (!serverAlive(backup)) {
+                replaceBackup(primary);
             }
-        } else if (!serverAlive(backup)) {
-            replaceBackup(primary);
+            prevAliveServers = aliveServers;
+            aliveServers = new HashSet<Address>();
         }
-        prevAliveServers = aliveServers;
-        aliveServers = new HashSet<Address>();
         set(t, PING_CHECK_MILLIS);
     }
 
@@ -98,30 +99,34 @@ class ViewServer extends Node {
     // Your code here...
 
     private boolean serverAlive(Address server) {
-        return aliveServers.contains(server) || prevAliveServers.contains(server);
+        return aliveServers.contains(server); // || prevAliveServers.contains(server);
     }
 
     private void replaceBackup(Address newPrimary) {
         Address newBackup = findNewBackup(newPrimary);
-        if (newBackup.compareTo(currentView.backup()) != 0 ||
-                newPrimary.compareTo(currentView.primary()) != 0) {
+        if (!Objects.equals(newBackup, currentView.backup()) ||
+                !Objects.equals(newPrimary, currentView.primary())) {
             // TODO: copy k-v to new backup.
+            if (!Objects.equals(newPrimary, currentView.primary())) {
+                ack = false;
+            }
             currentView = new View(currentView.viewNum() + 1, newPrimary, newBackup);
         }
     }
 
     private Address findNewBackup(Address primary) {
         for (Address server : aliveServers) {
-            if (server.compareTo(primary) != 0) {
+            if (!Objects.equals(server, primary)) {
                 return server;
             }
         }
 
-        for (Address server : prevAliveServers) {
-            if (server.compareTo(primary) != 0) {
-                return server;
-            }
-        }
+        // for (Address server : prevAliveServers) {
+        //     if (server.compareTo(primary) != 0) {
+        //         return server;
+        //     }
+        // }
+
         return null;
     }
 }
